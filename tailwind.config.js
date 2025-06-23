b3cba613bf9fd26bb2032265519c40c01929d9fb
Filip Tichý <filiptichy888@gmail.com>
2025-06-23 16:18:44 +0200

    snad to fnguje

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",    // cesty ke všem složkám, kde používáš tailwind třídy
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
